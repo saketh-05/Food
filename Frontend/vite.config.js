bab407b9f4e92a08864a16709335094cb7a01ff5
Saketh Surya <raisingsurya05@gmail.com>
2024-12-16 09:58:06 +0530

    git

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite';
 
 //import mkcert from 'vite-plugin-mkcert'
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
     strictPort: true,
-    http: true
   },
   build: {
     outDir: 'dist', // Ensure this matches your Dockerfile path
